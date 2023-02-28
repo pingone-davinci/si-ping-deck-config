fafe203c257e57a8b1ff28f1044847f201140c50
gmorgan-ping <gmorgan@pingidentity.com>
2023-02-28 12:28:07 -0400

    Update field focus js snippet

--- resources/codeSnippets/field-focus.js
+++ resources/codeSnippets/field-focus.js
@@ -1,5 +1,15 @@
-const myElement = document.getElementById("__ELEMENT_ID__")
+const focusOnFirstInputElement = () => {
+    document.getElementById("__ELEMENT_ID__").focus();
+};
 
-if (myElement) {
-    myElement.focus()
+/**
+ * If page isn't loaded yet, wait for the page to load, then focus on first
+ * input element.
+ */
+if (document.readyState === "loading") {
+    // Loading hasn't finished yet
+    document.addEventListener("DOMContentLoaded", focusOnFirstInputElement);
+} else {
+    // `DOMContentLoaded` has already fired
+    focusOnFirstInputElement();
 }
