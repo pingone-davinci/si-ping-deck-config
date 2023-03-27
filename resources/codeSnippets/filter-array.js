8def37b2069cdd4319b4020aa4caa55c14c05c7a
Glen Morgan <31349099+gmorgan-ping@users.noreply.github.com>
2023-03-27 11:20:27 -0300

    reformatting

--- resources/codeSnippets/filter-array.js
+++ resources/codeSnippets/filter-array.js
@@ -1,5 +1,8 @@
 // Example array of objects
-const __myArrayOfObjects__ = [{ type: "sms", value: "555-1212" }, { type: "email", value: "test@test.com" }, { type: "sms", value: "777-1212" }];
+const __myArrayOfObjects__ = [
+  { type: "sms", value: "555-1212" }, 
+  { type: "email", value: "test@test.com" }, 
+  { type: "sms", value: "777-1212" }];
 
 const filteredArray = __myArrayOfObjects__.filter(element => element.type === "sms");
 console.log(filteredArray); 
