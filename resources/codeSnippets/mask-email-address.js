fdb4487bf403820b52ef58a4adc64c1e7d77c5ce
gmorgan-ping <gmorgan@pingidentity.com>
2023-04-03 14:22:55 -0300

    Update email masking snippet

--- resources/codeSnippets/mask-email-address.js
+++ resources/codeSnippets/mask-email-address.js
@@ -1,5 +1,10 @@
 function maskEmail(email) {
-    const [name, domain] = email.split("@");
-    const maskedEmail = `${name[0]}${new Array(name.length).join("*")}@${domain}`;
-    return maskedEmail
+    const [username, domain] = email.split("@");
+    const maskedUsername =
+        username.charAt(0) + "*".repeat(username.length - 2) + username.slice(-1);
+    return maskedUsername + "@" + domain;
 }
+
+// Example Usage
+const maskedEmail = maskEmail("johndoe@gmail.com");
+console.log(maskedEmail); // Output: j********e@gmail.com
