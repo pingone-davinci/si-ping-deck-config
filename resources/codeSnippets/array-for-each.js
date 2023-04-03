4489e756e84fe134bf9c005392a29244445ae604
gmorgan-ping <gmorgan@pingidentity.com>
2023-04-03 14:50:04 -0300

    Update for each snippet

--- resources/codeSnippets/array-for-each.js
+++ resources/codeSnippets/array-for-each.js
@@ -1,4 +1,8 @@
-__myArrayOfObjects__.forEach((element, index, array) => {
-    console.log(element.type); // write the value
-    console.log(index); // 0, 1, 2
+// Iterate through each element in the array
+__myArrayOfObjects__.forEach((element, index) => {
+    // Output the value of the 'type' property for the current element
+    console.log(element.type);
+
+    // Output the index of the current element (0, 1, 2, etc.)
+    console.log(index);
 });
