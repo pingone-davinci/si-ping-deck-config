f14b0af999201bcc793a27849a48f22e1f4ef072
gmorgan-ping <gmorgan@pingidentity.com>
2023-04-03 13:50:46 -0300

    Update filter array

--- resources/codeSnippets/filter-array.js
+++ resources/codeSnippets/filter-array.js
@@ -1,9 +1,14 @@
 // Example array of objects
-const __myArrayOfObjects__ = [
+const objectArray = [
   { type: "sms", value: "555-1212" },
   { type: "email", value: "test@test.com" },
   { type: "sms", value: "777-1212" }
 ];
 
-const filteredArray = __myArrayOfObjects__.filter(element => element.type === "sms");
-console.log(filteredArray); 
+// Filter the array to include only objects with type "sms"
+const filteredArray = objectArray.filter(
+  (object) => object.type === "sms"
+);
+
+// Log the filtered array to the console
+console.log(filteredArray);
